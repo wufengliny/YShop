f8fea34470a6aec6904ae1300aef701becd482d7
Kay <59470902+kaymingb@users.noreply.github.com>
2020-02-04 18:56:37 +0800

    11

---
+++ YForm/Collect37Img.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace YForm
+{
+    public partial class Collect37Img : Form
+    {
+        public Collect37Img()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string str = " select * from AMH_Vedio ";
+            DataTable dt= new Yax.BLL.BCommon().GetDataBySQL(str);
+            for(int i=0;i<dt.Rows.Count;i++)
+            {
+                string html = Yax.Common.HTTPHelper.GetHTMLUTF8(dt.Rows[i]["FromPageUrl"].ToString());
+
+            }
+            MessageBox.Show(dt.Rows[0]["Name"].ToString());
+        }
+    }
+}
--- YForm/Program.cs
+++ YForm/Program.cs
@@ -16,7 +16,7 @@ namespace YForm
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Collect37Img());
         }
     }
 }
