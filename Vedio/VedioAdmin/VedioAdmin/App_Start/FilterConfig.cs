cc00c1b9f95c7df867b5a46a8e303472a1f18187
Kay <59470902+kaymingb@users.noreply.github.com>
2020-07-02 18:03:55 +0800

    11

---
+++ Vedio/VedioAdmin/VedioAdmin/App_Start/FilterConfig.cs
@@ -0,0 +1,17 @@
+﻿
+using VedioAdmin.Filters;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace VedioAdmin
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+            filters.Add(new ExceptionFilter());
+        }
+    }
+}
