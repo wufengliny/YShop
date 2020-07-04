672db3aee00ffcbaa405659d78fa29b1225f51d6
Kay <59470902+kaymingb@users.noreply.github.com>
2020-07-04 15:12:05 +0800

    11

--- Vedio/VedioAdmin/DAL/DC_Vedios.cs
+++ Vedio/VedioAdmin/DAL/DC_Vedios.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CommSQLHelper;
+using Entity;
+
 
 namespace DAL
 {
     public class DC_Vedios
     {
+
+        public MC_Vedios GetModelByID(int ID)
+        {
+            string str = "select * from C_Vedios where ID=@ID";
+            SqlParameter param = new SqlParameter("@ID", SqlDbType.Int, 4);
+            param.Value = ID;
+            return SQLHelper.ExecuteReaderObject<MC_Vedios>(CommandType.Text, str, param);
+        }
     }
 }
