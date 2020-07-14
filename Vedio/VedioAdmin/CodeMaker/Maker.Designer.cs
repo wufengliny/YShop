80d0683649eb9da3ac65ae875e0e0bdc595e5326
xiaoka <kaymingming@gmail.com>
2020-07-14 08:42:29 +0800

    11

--- Vedio/VedioAdmin/CodeMaker/Maker.Designer.cs
+++ Vedio/VedioAdmin/CodeMaker/Maker.Designer.cs
@@ -32,29 +32,28 @@
             this.btnColse = new System.Windows.Forms.Button();
             this.btnConn = new System.Windows.Forms.Button();
             this.txtConn = new System.Windows.Forms.TextBox();
+            this.cbb_conns = new System.Windows.Forms.ComboBox();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.cbb_conns);
             this.groupBox1.Controls.Add(this.btnColse);
             this.groupBox1.Controls.Add(this.btnConn);
             this.groupBox1.Controls.Add(this.txtConn);
-            this.groupBox1.Location = new System.Drawing.Point(31, 46);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Location = new System.Drawing.Point(21, 31);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
-            this.groupBox1.Size = new System.Drawing.Size(848, 225);
+            this.groupBox1.Size = new System.Drawing.Size(565, 150);
             this.groupBox1.TabIndex = 12;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "数据库连接：";
             // 
             // btnColse
             // 
-            this.btnColse.Location = new System.Drawing.Point(497, 112);
-            this.btnColse.Margin = new System.Windows.Forms.Padding(4);
+            this.btnColse.Location = new System.Drawing.Point(387, 103);
             this.btnColse.Name = "btnColse";
-            this.btnColse.Size = new System.Drawing.Size(112, 44);
+            this.btnColse.Size = new System.Drawing.Size(75, 29);
             this.btnColse.TabIndex = 11;
             this.btnColse.Text = "关闭";
             this.btnColse.UseVisualStyleBackColor = true;
@@ -62,29 +61,37 @@
             // 
             // btnConn
             // 
-            this.btnConn.Location = new System.Drawing.Point(188, 112);
-            this.btnConn.Margin = new System.Windows.Forms.Padding(4);
+            this.btnConn.Location = new System.Drawing.Point(77, 103);
             this.btnConn.Name = "btnConn";
-            this.btnConn.Size = new System.Drawing.Size(112, 44);
+            this.btnConn.Size = new System.Drawing.Size(75, 29);
             this.btnConn.TabIndex = 10;
             this.btnConn.Text = "确定";
             this.btnConn.UseVisualStyleBackColor = true;
+            this.btnConn.Click += new System.EventHandler(this.btnConn_Click);
             // 
             // txtConn
             // 
-            this.txtConn.Location = new System.Drawing.Point(31, 29);
-            this.txtConn.Margin = new System.Windows.Forms.Padding(4);
+            this.txtConn.Location = new System.Drawing.Point(21, 19);
             this.txtConn.Multiline = true;
             this.txtConn.Name = "txtConn";
-            this.txtConn.Size = new System.Drawing.Size(793, 60);
+            this.txtConn.Size = new System.Drawing.Size(530, 53);
             this.txtConn.TabIndex = 9;
+            // 
+            // cbb_conns
+            // 
+            this.cbb_conns.FormattingEnabled = true;
+            this.cbb_conns.Location = new System.Drawing.Point(217, 103);
+            this.cbb_conns.Name = "cbb_conns";
+            this.cbb_conns.Size = new System.Drawing.Size(121, 20);
+            this.cbb_conns.TabIndex = 12;
             // 
             // Maker
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(906, 333);
+            this.ClientSize = new System.Drawing.Size(604, 222);
             this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Maker";
             this.Text = "Maker";
             this.groupBox1.ResumeLayout(false);
@@ -99,5 +106,6 @@
         private System.Windows.Forms.Button btnColse;
         private System.Windows.Forms.Button btnConn;
         private System.Windows.Forms.TextBox txtConn;
+        private System.Windows.Forms.ComboBox cbb_conns;
     }
 }
--- Vedio/VedioAdmin/CodeMaker/Maker.cs
+++ Vedio/VedioAdmin/CodeMaker/Maker.cs
@@ -15,11 +15,37 @@ namespace CodeMaker
         public Maker()
         {
             InitializeComponent();
+            string defaultConn= System.Configuration.ConfigurationManager.AppSettings["defaultconn"];
+            var list = System.Configuration.ConfigurationManager.AppSettings;
+            List<string> listcons = new List<string>();
+            foreach (var item in list)
+            {
+                if (item.ToString().StartsWith("conn"))
+                {
+                    this.cbb_conns.Items.Add(item); ;
+                }
+            }
+            this.cbb_conns.SelectedItem = defaultConn;
         }
 
         private void btnColse_Click(object sender, EventArgs e)
         {
             Application.Exit();
+        }
+
+        private void btnConn_Click(object sender, EventArgs e)
+        {
+            var list = System.Configuration.ConfigurationManager.AppSettings;
+            List<string> listcons = new List<string>();
+            foreach(var item in list)
+            {
+                if(item.ToString().StartsWith("conn"))
+                {
+                    listcons.Add(item.ToString());
+                    string txtval = System.Configuration.ConfigurationManager.AppSettings[item.ToString()];
+                }
+            }
+
         }
     }
 }
